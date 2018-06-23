99def164a1e00c54735d72add06f909d25bf304b
bergi <bergi@axolotlfarm.org>
2018-06-23 02:10:29 +0200

    initial commit

---
+++ examples/respeaker-4mic-array.js
@@ -0,0 +1,30 @@
+const pixelRing = require('..').reSpeaker4MicArrayPixelRing
+
+const ring = pixelRing(pixelRing())
+
+ring.init().then(() => {
+  const floatMod = (n, m) => {
+    return n - Math.floor(n / m) * m
+  }
+
+  ring.animate(ring.length, 4000, (f) => {
+    const p0 = floatMod(ring.length * 10 - f * 4, ring.length)
+    const p1 = floatMod(f, ring.length)
+
+    for (let index = 0; index < ring.length; index++) {
+      const value0 = floatMod(p0 - index + ring.length, ring.length) / (ring.length - 1)
+      const value1 = floatMod(p1 - index + ring.length, ring.length) / (ring.length - 1)
+
+      ring.color(index, value0 * 255 - 128, 0, value1 * 255 - 128, 255)
+    }
+  })
+}).catch(err => {
+  console.error(err)
+})
+
+process.on('SIGINT', () => {
+  ring.stop().then(() => process.exit())
+})
+
+console.log('Shows a red and a blue colored rotating pattern on the LED ring.')
+console.log('Exit the program with Ctrl-C.')
---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  reSpeakerCoreV2PixelRing: require('./lib/ReSpeakerCoreV2PixelRing').create,
+  reSpeaker4MicArrayPixelRing: require('./lib/ReSpeaker4MicArrayPixelRing').create,
+  Apa102: require('./lib/Apa102')
+}
